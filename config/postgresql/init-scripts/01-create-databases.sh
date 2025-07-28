e8430ea7bf42e486f5a8df71d3bd3b3bcf07a605
Joe Broxson <joe@improbablestudios.com>
2025-07-28 13:11:27 -0700

    Eliminate LiteLLM DB errors

--- config/postgresql/init-scripts/01-create-databases.sh
+++ config/postgresql/init-scripts/01-create-databases.sh
@@ -12,11 +12,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE n8n;
     GRANT ALL PRIVILEGES ON DATABASE n8n TO $POSTGRES_USER;
     
-    -- Create litellm database
-    CREATE DATABASE litellm;
-    CREATE SCHEMA IF NOT EXISTS litellm;
-    GRANT ALL PRIVILEGES ON DATABASE litellm TO $POSTGRES_USER;
-    
     -- Create additional databases if needed
     -- CREATE DATABASE other_service;
     -- GRANT ALL PRIVILEGES ON DATABASE other_service TO $POSTGRES_USER;
